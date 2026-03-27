a3b8e168b3dd6e1b862e2fe985f7499e1bf20146
Italo Chagas <felipe.italo@hotmail.com>
2026-03-27 10:55:37 -0300

    refactor(user): share authenticated context core

--- app/graphql/queries/user.py
+++ app/graphql/queries/user.py
@@ -2,8 +2,11 @@ from __future__ import annotations
 
 import graphene
 
+from app.application.services.authenticated_user_context_service import (
+    AuthenticatedUserContextService,
+)
 from app.graphql.auth import get_current_user_required
-from app.graphql.schema_utils import _user_to_graphql_payload
+from app.graphql.authenticated_user_presenters import to_authenticated_user_type
 from app.graphql.types import UserType
 
 
@@ -12,4 +15,5 @@ class UserQueryMixin:
 
     def resolve_me(self, _info: graphene.ResolveInfo) -> UserType:
         user = get_current_user_required()
-        return UserType(**_user_to_graphql_payload(user))
+        profile = AuthenticatedUserContextService.with_defaults().build_profile(user)
+        return to_authenticated_user_type(profile)
